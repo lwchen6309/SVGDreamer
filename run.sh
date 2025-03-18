cff09de6b294970212bf159aa6e1315d9d5d2d95
lwchen <li-wei.chen@kuleuven.be>
2025-03-18 14:07:47 +0100

    Set sigma to 50

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 # python svgdreamer.py x=iconography skip_sive=False "prompt='A poster of the great wall, teal and orange color scheme, autumn colors'" token_ind=4 x.vpsd.t_schedule='randint' result_path='./logs/great_wall' x.xing_loss.weight=0. x.num_paths=512 +target_file="./init_target/demo/example_0072_demo.svg" x.composition_loss.weight=0. mv=True
 
 weight=5e2
+sigma=75
 composition_type=("golden_spiral" "pyramid" "diagonal" "l_shape")
 # composition_type=("golden_spiral")
 for composition in "${composition_type[@]}"; do
     python svgdreamer.py x=iconography skip_sive=False "prompt='A poster of the great wall, teal and orange color scheme, autumn colors'" \
     token_ind=4 x.vpsd.t_schedule='randint' result_path='./logs/great_wall' \
     x.xing_loss.weight=0. x.num_paths=512 +target_file="./init_target/demo/example_0072_demo.svg" \
-    x.composition_loss.composition_type=$composition x.composition_loss.weight=$weight x.composition_loss.sigma=50. \
+    x.composition_loss.composition_type=$composition x.composition_loss.weight=$weight x.composition_loss.sigma=$sigma \
     x.sam_composition_loss.weight=$weight
 done
